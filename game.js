dfac673cdb0c1111578b9a7b861076993ffcb5de
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-05-06 20:57:46 -0400

    Pruebo algunos atributos y funciones de canvas para hacer líneas y texto

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame)
+
+function startGame() {
+    // game.fillRect(0, 0, 100, 100);
+    // game.clearRect(0, 0, 50, 50);
+
+    game.font = '24px Verdana';
+    game.fillStyle = 'purple';
+    game.textAlign = 'start';
+    game.fillText('Platzi', 25, 25);
+}
