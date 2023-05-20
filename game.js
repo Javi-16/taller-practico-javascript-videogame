23f4f1ed7c536292ee8f066e137a098613da70c4
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-05-19 23:38:03 -0400

    Agrego la detección de las coliciones con las bombas

--- game.js
+++ game.js
@@ -15,7 +15,8 @@ const playerPosition = {
 const giftPosition = {
     x: undefined,
     y: undefined
-}
+};
+let enemyPositions = [];
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -47,7 +48,9 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({map, mapRows, mapRowCols});
 
+    enemyPositions = [];
     game.clearRect(0, 0, canvasSize, canvasSize);
+
     mapRowCols.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
             const emoji = emojis[col];
@@ -63,6 +66,12 @@ function startGame() {
             else if (col === 'I') {
                 giftPosition.x = posX;
                 giftPosition.y = posY;
+            }
+            else if (col === 'X') {
+                enemyPositions.push({
+                    x: posX,
+                    y: posY
+                });
             }
 
             game.fillText(emoji, posX, posY);
@@ -85,6 +94,16 @@ function movePlayer() {
 
     if (giftCollision) {
         console.log('Subiste de nivel');
+    }
+
+    const enemyCollision = enemyPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(3) === playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) === playerPosition.y.toFixed(3);
+        return enemyCollisionX && enemyCollisionY;
+    });
+
+    if (enemyCollision) {
+        console.log('Chocaste contra una bomba');
     }
 
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
